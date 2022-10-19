036d3a58d15f6a91aa3a46d68998f4cfd0c94655
yandens <dedensetyawan17120104@gmail.com>
2022-10-19 19:01:09 +0800

    testing github actions

--- api/product.js
+++ api/product.js
@@ -3,9 +3,20 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   try {
-    res.json({
+    return res.json({
       status: 200,
       message: 'Get Data Successfully'
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})
+
+router.get('/test', async (req, res) => {
+  try {
+    return res.json({
+      status: 200,
+      message: 'Testing Github Actions'
     })
   } catch (err) {
     console.log(err)
